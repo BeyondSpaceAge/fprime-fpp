2aaba57a60f943173e6635d8bbcaecd08335a048
Sourcery AI <>
2022-06-01 20:06:32 +0000

    'Refactored by Sourcery'

--- fprime_fpp_install.py
+++ fprime_fpp_install.py
@@ -179,9 +179,9 @@ def github_release_download(version: str):
         wget(release_url)
     except urllib.error.HTTPError as error:
         retry_likely = "404" not in str(error)
-        retry_error = f"Retrying will likely resolve the problem."
         # Check if this is a real error or not available error
         if retry_likely:
+            retry_error = "Retrying will likely resolve the problem."
             print(f"-- INFO  -- { retry_error }", file=sys.stderr)
             sys.exit(-1)
 
@@ -286,7 +286,6 @@ def clean_install_fpp():
 
     def lazy_loader():
         """Prevents the download of FPP items until actually enumerated"""
-        for item in iterate_fpp_tools(WORKING_DIR):
-            yield item
+        yield from iterate_fpp_tools(WORKING_DIR)
 
     yield lazy_loader()
